d3f05c686401a8213e60ea0081b40c0e3c77416a
Ryan Lee <drdgvhbh@gmail.com>
2019-05-13 20:36:47 -0400

    Fix missing string type annotation on title in dialog provider

--- file_picker/dialogs.go
+++ file_picker/dialogs.go
@@ -3,11 +3,11 @@ package file_picker
 import "github.com/gen2brain/dlgs"
 
 type dialog interface {
-	File(title, filter string, directory bool) (string, bool, error)
+	File(title string, filter string, directory bool) (string, bool, error)
 }
 
 type dialogProvider struct {}
 
-func (dialog dialogProvider) File(title, filter string, directory bool) (string, bool, error) {
+func (dialog dialogProvider) File(title string, filter string, directory bool) (string, bool, error) {
 	return dlgs.File(title, filter, directory)
 }
