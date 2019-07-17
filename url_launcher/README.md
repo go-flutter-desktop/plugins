66f67dbc737708b1028862e5ea465e8f034e5bbb
chunhunghan <chunhunghan@gmail.com>
2019-07-17 09:12:06 +0800

    update review request

--- url_launcher/README.md
+++ url_launcher/README.md
@@ -1,1 +1,24 @@
 # url_launcher
+
+This Go package implements the host-side of the Flutter [url_launcher](https://github.com/flutter/plugins/tree/master/packages/url_launcher) plugin.
+
+## Usage
+
+Import as:
+
+```go
+import "github.com/go-flutter-desktop/plugins/url_launcher"
+```
+
+Then add the following option to your go-flutter [application options](https://github.com/go-flutter-desktop/go-flutter/blob/68868301742b864b719b31ae51c7ec4b3b642d1a/example/simpleDemo/main.go#L53):
+
+```go
+flutter.AddPlugin(&url_launcher.UrlLauncherPlugin{}),
+```
+
+Change the values of the Vendor and Application names to a custom and unique
+string, so it doesn't conflict with other organizations.
+
+## Issues
+
+Please report issues at the [go-flutter issue tracker](https://github.com/go-flutter-desktop/go-flutter/issues/).
--- url_launcher/plugin.go
+++ url_launcher/plugin.go
@@ -20,26 +20,58 @@ func (p *UrlLauncherPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
 	channel.HandleFunc("launch", p.launch)
 	channel.HandleFunc("canLaunch", p.canLaunch)
+
+	// Ignored: The plugins doesn't handle WebView.
+	// This call will not do anything, because there is no WebView to close.
+	channel.HandleFunc("closeWebView", func(_ interface{}) (interface{}, error) { return nil, nil })
 	return nil
 }
 
 func (p *UrlLauncherPlugin) launch(arguments interface{}) (reply interface{}, err error) {
-	fmt.Println("launch")
 	var url string
+	var useWebView bool
+	var useSafariVC bool
+	var enableJavaScript bool
+	var enableDomStorage bool
+	var universalLinksOnly bool
 
 	argsMap := arguments.(map[interface{}]interface{})
 	url = argsMap["url"].(string)
 	if url == "" {
 		return nil, errors.New("url is empty")
 	}
+
+	useWebView = argsMap["useWebView"].(bool)
+	if useWebView == true {
+		fmt.Println("plugins.flutter.io/url_launcher: WebView aren't supported on desktop.")
+	}
+
+	useSafariVC = argsMap["useSafariVC"].(bool)
+	if useSafariVC == true {
+		fmt.Println("plugins.flutter.io/url_launcher: SafariVC aren't supported on desktop.")
+	}
+
+	enableJavaScript = argsMap["enableJavaScript"].(bool)
+	if enableJavaScript == true {
+		fmt.Println("plugins.flutter.io/url_launcher: enableJavaScript aren't supported on desktop.")
+	}
+
+	enableDomStorage = argsMap["enableDomStorage"].(bool)
+	if enableDomStorage == true {
+		fmt.Println("plugins.flutter.io/url_launcher: enableDomStorage aren't supported on desktop.")
+	}
+
+	universalLinksOnly = argsMap["universalLinksOnly"].(bool)
+	if universalLinksOnly == true {
+		fmt.Println("plugins.flutter.io/url_launcher: universalLinksOnly aren't supported on desktop.")
+	}
+
 	browser.OpenURL(url)
-	//ignore 'useSafariVC' , 'useWebView' , 'enableJavaScript' , 'enableDomStorage' , 'universalLinksOnly'
 
 	return nil, nil
 }
 
 func (p *UrlLauncherPlugin) canLaunch(arguments interface{}) (reply interface{}, err error) {
-	fmt.Println("canLaunch")
 	var url string
 
 	argsMap := arguments.(map[interface{}]interface{})
@@ -47,7 +79,6 @@ func (p *UrlLauncherPlugin) canLaunch(arguments interface{}) (reply interface{},
 	if url == "" {
 		return false, errors.New("url is empty")
 	}
-	//windows can accept mailto: sms: tel: notes: , ignore this check condition strings.HasPrefix(url, "?:")
 
 	return true, nil
 }
